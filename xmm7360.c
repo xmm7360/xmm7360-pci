a723b4170365f8d8c94cbf4aa6135f4882b9d718
James Wah <james@laird-wah.net>
2019-12-21 22:55:46 +1100

    Bring up TD rings
    This is still somewhat crude, but it's possible to talk to the RPC
    channel on /dev/xmm1 now.

--- xmm7360.c
+++ xmm7360.c
@@ -6,6 +6,9 @@
 #include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/delay.h>
+#include <linux/uaccess.h>
+#include <linux/cdev.h>
+#include <linux/wait.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -15,17 +18,20 @@ static struct pci_device_id xmm7360_ids[] = {
 };
 MODULE_DEVICE_TABLE(pci, xmm7360_ids);
 
+static dev_t xmm_base;
+
 /* Command ring, which is used to configure the queue pairs */
 struct cmd_ring_entry {
 	dma_addr_t ptr;
 	u16 len;
 	u8 parm;
 	u8 cmd;
-	u32 unk1, unk2, flags;
+	u32 extra;
+	u32 unk, flags;
 };
 
-#define CMD_QP_OPEN	1
-#define CMD_QP_CLOSE	3
+#define CMD_RING_OPEN	1
+#define CMD_RING_CLOSE	3
 #define CMD_WAKEUP	4
 
 #define CMD_FLAG_DONE	1
@@ -79,7 +85,7 @@ struct control_page {
 #define BAR0_DOORBELL	0x04
 #define BAR0_WAKEUP	0x14
 
-#define DOORBELL_QP	0
+#define DOORBELL_TD	0
 #define DOORBELL_CMD	1
 
 #define BAR2_STATUS	0x00
@@ -92,6 +98,29 @@ struct control_page {
 #define BAR2_BLANK2	0x1d
 #define BAR2_BLANK3	0x1e
 
+/* There are 16 TD rings: a Tx and Rx ring for each queue pair */
+struct td_ring {
+	u8 size;
+	u8 last_handled;
+	u16 page_size;
+
+	struct td_ring_entry *tds;
+	dma_addr_t tds_phys;
+
+	// One page of page_size per td
+	void **pages;
+	dma_addr_t *pages_phys;
+};
+
+struct queue_pair {
+	struct xmm_dev *xmm;
+	struct cdev cdev;
+	struct device dev;
+	int num;
+	int open;
+	wait_queue_head_t wq;
+	spinlock_t lock;
+};
 
 struct xmm_dev {
 	struct device *dev;
@@ -100,15 +129,20 @@ struct xmm_dev {
 	volatile uint32_t *bar0, *bar2;
 
 	int irq[4];
+	wait_queue_head_t wq;
 
 	volatile struct control_page *cp;
 	dma_addr_t cp_phys;
 
+	struct td_ring td_ring[16];
+
+	struct queue_pair qp[8];
 };
 
 static void xmm7360_dump(struct xmm_dev *xmm)
 {
 	pr_info("xmm %08x slp %d cmd %d:%d\n", xmm->cp->status.code, xmm->cp->status.asleep, xmm->cp->c_rptr, xmm->cp->c_wptr);
+	pr_info("xmm r2 %d:%d r3 %d:%d\n", xmm->cp->s_rptr[2], xmm->cp->s_wptr[2], xmm->cp->s_rptr[3], xmm->cp->s_wptr[3]);
 }
 
 static void xmm7360_ding(struct xmm_dev *xmm, int bell)
@@ -118,7 +152,13 @@ static void xmm7360_ding(struct xmm_dev *xmm, int bell)
 	xmm->bar0[BAR0_DOORBELL] = bell;
 }
 
-static int xmm7360_cmd_ring_submit(struct xmm_dev *xmm, u8 cmd, u8 parm, u16 len, dma_addr_t ptr)
+static int xmm7360_cmd_ring_wait(struct xmm_dev *xmm)
+{
+	// Wait for all commands to complete
+	return wait_event_interruptible(xmm->wq, xmm->cp->c_rptr == xmm->cp->c_wptr);
+}
+
+static int xmm7360_cmd_ring_submit(struct xmm_dev *xmm, u8 cmd, u8 parm, u16 len, dma_addr_t ptr, u32 extra)
 {
 	u8 wptr = xmm->cp->c_wptr;
 	u8 new_wptr = (wptr + 1) % CMD_RING_SIZE;
@@ -131,8 +171,8 @@ static int xmm7360_cmd_ring_submit(struct xmm_dev *xmm, u8 cmd, u8 parm, u16 len
 	xmm->cp->c_ring[wptr].cmd = cmd;
 	xmm->cp->c_ring[wptr].parm = parm;
 	xmm->cp->c_ring[wptr].len = len;
-	xmm->cp->c_ring[wptr].unk1 = 0;
-	xmm->cp->c_ring[wptr].unk2 = 0;
+	xmm->cp->c_ring[wptr].extra = extra;
+	xmm->cp->c_ring[wptr].unk = 0;
 	xmm->cp->c_ring[wptr].flags = CMD_FLAG_READY;
 
 	xmm->cp->c_wptr = new_wptr;
@@ -181,15 +221,23 @@ static int xmm7360_cmd_ring_init(struct xmm_dev *xmm) {
 	if (!timeout)
 		return -ETIMEDOUT;
 
-	ret = xmm7360_cmd_ring_submit(xmm, CMD_WAKEUP, 0, 1, 0);
+	ret = xmm7360_cmd_ring_submit(xmm, CMD_WAKEUP, 0, 1, 0, 0);
 	if (ret)
 		return ret;
-	ret = xmm7360_cmd_ring_submit(xmm, 0xf0, 0x80, 0, 0);
+	ret = xmm7360_cmd_ring_submit(xmm, 0xf0, 0x80, 0, 0, 0);
 	if (ret)
 		return ret;
+
+	xmm7360_dump(xmm);
 
 	xmm7360_ding(xmm, DOORBELL_CMD);
+	xmm7360_dump(xmm);
 
+	ret = xmm7360_cmd_ring_wait(xmm);
+	if (ret)
+		return ret;
+
+	xmm7360_dump(xmm);
 	return 0;
 }
 
@@ -202,12 +250,277 @@ static void xmm7360_cmd_ring_free(struct xmm_dev *xmm) {
 	return;
 }
 
+static void xmm7360_td_ring_create(struct xmm_dev *xmm, u8 ring_id, u8 size)
+{
+	struct td_ring *ring = &xmm->td_ring[ring_id];
+	int i;
+
+	BUG_ON(ring->size);
+	BUG_ON(size & (size-1));
+
+	memset(ring, 0, sizeof(struct td_ring));
+	ring->size = size;
+	ring->page_size = 0x1000;
+	ring->tds = dma_alloc_coherent(xmm->dev, sizeof(struct td_ring_entry)*size, &ring->tds_phys, GFP_KERNEL);
+
+	ring->pages = kzalloc(sizeof(void*)*size, GFP_KERNEL);
+	ring->pages_phys = kzalloc(sizeof(dma_addr_t)*size, GFP_KERNEL);
+
+	for (i=0; i<size; i++) {
+		ring->pages[i] = dma_alloc_coherent(xmm->dev, ring->page_size, &ring->pages_phys[i], GFP_KERNEL);
+		ring->tds[i].addr = ring->pages_phys[i];
+	}
+
+	xmm->cp->s_rptr[ring_id] = xmm->cp->s_wptr[ring_id] = 0;
+	xmm7360_cmd_ring_submit(xmm, CMD_RING_OPEN, ring_id, size, ring->tds_phys, 0x60);
+	xmm7360_ding(xmm, DOORBELL_CMD);
+}
+
+static void xmm7360_td_ring_destroy(struct xmm_dev *xmm, u8 ring_id)
+{
+	struct td_ring *ring = &xmm->td_ring[ring_id];
+	int i, size=ring->size;
+
+	if (!size) {
+		WARN_ON(1);
+		pr_err("Tried destroying empty ring!\n");
+		return;
+	}
+
+	xmm7360_cmd_ring_submit(xmm, CMD_RING_CLOSE, ring_id, 0, 0, 0);
+	xmm7360_ding(xmm, DOORBELL_CMD);
+
+	for (i=0; i<size; i++) {
+		dma_free_coherent(xmm->dev, ring->page_size, ring->pages[i], ring->pages_phys[i]);
+	}
+
+	kfree(ring->pages_phys);
+	kfree(ring->pages);
+
+	dma_free_coherent(xmm->dev, sizeof(struct td_ring_entry)*size, ring->tds, ring->tds_phys);
+
+	ring->size = 0;
+}
+
+static void xmm7360_td_ring_write_user(struct xmm_dev *xmm, u8 ring_id, const void __user *buf, size_t len)
+{
+	struct td_ring *ring = &xmm->td_ring[ring_id];
+	u8 wptr = xmm->cp->s_wptr[ring_id];
+
+	BUG_ON(!ring->size);
+	BUG_ON(len > ring->page_size);
+	BUG_ON(ring_id & 1);
+
+	copy_from_user(ring->pages[wptr], buf, len);
+	ring->tds[wptr].length = len;
+	ring->tds[wptr].flags = 0;
+	ring->tds[wptr].unk = 0;
+
+	print_hex_dump(KERN_INFO, "xmm write ", DUMP_PREFIX_OFFSET, 8, 16, ring->pages[wptr], len, 1);
+
+	wptr = (wptr + 1) & (ring->size - 1);
+	BUG_ON(wptr == xmm->cp->s_rptr[ring_id]);
+
+	xmm->cp->s_wptr[ring_id] = wptr;
+}
+
+static void xmm7360_td_ring_write(struct xmm_dev *xmm, u8 ring_id, const void *buf, int len)
+{
+	struct td_ring *ring = &xmm->td_ring[ring_id];
+	u8 wptr = xmm->cp->s_wptr[ring_id];
+
+	BUG_ON(!ring->size);
+	BUG_ON(len > ring->page_size);
+	BUG_ON(ring_id & 1);
+
+	memcpy(ring->pages[wptr], buf, len);
+	ring->tds[wptr].length = len;
+	ring->tds[wptr].flags = 0;
+	ring->tds[wptr].unk = 0;
+
+	wptr = (wptr + 1) & (ring->size - 1);
+	BUG_ON(wptr == xmm->cp->s_rptr[ring_id]);
+
+	xmm->cp->s_wptr[ring_id] = wptr;
+}
+
+static int xmm7360_td_ring_full(struct xmm_dev *xmm, u8 ring_id)
+{
+	struct td_ring *ring = &xmm->td_ring[ring_id];
+	u8 wptr = xmm->cp->s_wptr[ring_id];
+	wptr = (wptr + 1) & (ring->size - 1);
+	return wptr == xmm->cp->s_rptr[ring_id];
+}
+
+static void xmm7360_td_ring_read(struct xmm_dev *xmm, u8 ring_id)
+{
+	struct td_ring *ring = &xmm->td_ring[ring_id];
+	u8 wptr = xmm->cp->s_wptr[ring_id];
+
+	if (!ring->size) {
+		pr_err("read on disabled ring\n");
+		WARN_ON(1);
+		return;
+	}
+	if (!(ring_id & 1)) {
+		pr_err("read on write ring\n");
+		WARN_ON(1);
+		return;
+	}
+
+	ring->tds[wptr].length = ring->page_size;
+	ring->tds[wptr].flags = 0;
+	ring->tds[wptr].unk = 0;
+
+	wptr = (wptr + 1) & (ring->size - 1);
+	BUG_ON(wptr == xmm->cp->s_rptr[ring_id]);
+
+	xmm->cp->s_wptr[ring_id] = wptr;
+}
+
+static int xmm7360_qp_start(struct queue_pair *qp)
+{
+	struct xmm_dev *xmm = qp->xmm;
+	int ret;
+
+	spin_lock(&qp->lock);
+
+	if (qp->open) {
+		ret = -EBUSY;
+	} else {
+		ret = 0;
+		qp->open = 1;
+
+		pr_info("xmm: opening qp %d\n", qp->num);
+		xmm7360_td_ring_create(xmm, qp->num*2, 8);
+		xmm7360_td_ring_create(xmm, qp->num*2+1, 8);
+		xmm7360_ding(xmm, DOORBELL_CMD);
+		while (!xmm7360_td_ring_full(xmm, qp->num*2+1))
+			xmm7360_td_ring_read(xmm, qp->num*2+1);
+		xmm7360_ding(xmm, DOORBELL_TD);
+	}
+
+	spin_unlock(&qp->lock);
+
+	return ret;
+}
+
+static int xmm7360_qp_stop(struct queue_pair *qp)
+{
+	struct xmm_dev *xmm = qp->xmm;
+	int ret = 0;
+
+	spin_lock(&qp->lock);
+	if (!qp->open) {
+		ret = -ENODEV;
+	} else {
+		ret = 0;
+		qp->open = 0;
+
+		xmm7360_td_ring_destroy(xmm, qp->num*2);
+		xmm7360_td_ring_destroy(xmm, qp->num*2+1);
+		pr_info("xmm: closing qp %d\n", qp->num);
+	}
+	spin_unlock(&qp->lock);
+	return ret;
+}
+
+static size_t xmm7360_qp_write_user(struct queue_pair *qp, const char __user *buf, size_t size)
+{
+	struct xmm_dev *xmm = qp->xmm;
+	if (xmm7360_td_ring_full(xmm, qp->num*2))
+		return -ENOSPC;
+	pr_info("xmm7360_write: %ld bytes to qp %d\n", size, qp->num);
+	xmm7360_td_ring_write_user(xmm, qp->num*2, buf, size);
+	xmm7360_ding(xmm, DOORBELL_TD);
+	return 0;
+}
+
+static size_t xmm7360_qp_read_user(struct queue_pair *qp, char __user *buf, size_t size)
+{
+	struct xmm_dev *xmm = qp->xmm;
+	struct td_ring *ring = &xmm->td_ring[qp->num*2+1];
+	int idx, nread, ret;
+	pr_err("xmm7360_qp_read_user: initial rptr %d, lh %d\n", xmm->cp->s_rptr[qp->num*2+1], ring->last_handled);
+	ret = wait_event_interruptible(qp->wq, xmm->cp->s_rptr[qp->num*2+1] != ring->last_handled);
+	if (ret)
+		return ret;
+	pr_err("xmm7360_qp_read_user: mid rptr %d, lh %d\n", xmm->cp->s_rptr[qp->num*2+1], ring->last_handled);
+
+	idx = ring->last_handled;
+	nread = ring->tds[idx].length;
+	pr_err("Ring length: %x Requested length: %lx Page size: %x\n",  nread, size, ring->page_size);
+	if (nread > size)
+		nread = size;
+	copy_to_user(buf, ring->pages[idx], nread);
+	//print_hex_dump(KERN_INFO, "xmm read ", DUMP_PREFIX_OFFSET, 8, 16, ring->pages[idx], ring->trbs[idx].length, 1);
+
+	xmm7360_td_ring_read(xmm, qp->num*2+1);
+	xmm7360_ding(xmm, DOORBELL_TD);
+	ring->last_handled = (idx + 1) & (ring->size - 1);
+	return nread;
+}
+
+int xmm7360_open (struct inode *inode, struct file *file)
+{
+	struct queue_pair *qp = container_of(inode->i_cdev, struct queue_pair, cdev);
+	file->private_data = qp;
+	pr_info("xmm7360_open %d\n", qp->num);
+	return xmm7360_qp_start(qp);
+}
+
+int xmm7360_release (struct inode *inode, struct file *file)
+{
+	struct queue_pair *qp = file->private_data;
+	pr_info("xmm7360_release %d\n", qp->num);
+	return xmm7360_qp_stop(qp);
+}
+
+ssize_t xmm7360_write (struct file *file, const char __user *buf, size_t size, loff_t *offset)
+{
+	struct queue_pair *qp = file->private_data;
+	int ret;
+
+	pr_info("xmm7360_write %d %ld\n", qp->num, size);
+	ret = xmm7360_qp_write_user(qp, buf, size);
+	if (ret)
+		return ret;
+
+	*offset += size;
+	return size;
+}
+
+ssize_t xmm7360_read (struct file *file, char __user *buf, size_t size, loff_t *offset)
+{	struct queue_pair *qp = file->private_data;
+
+	size_t ret = xmm7360_qp_read_user(qp, buf, size);
+	if (ret < 0)
+		return ret;
+	*offset += ret;
+	return ret;
+}
+
+static struct file_operations xmm7360_fops = {
+	.read		= xmm7360_read,
+	.write		= xmm7360_write,
+	.open		= xmm7360_open,
+	.release	= xmm7360_release
+};
+
 static irqreturn_t xmm7360_irq0(int irq, void *dev_id) {
 	struct xmm_dev *xmm = dev_id;
 	int id;
 
 	pr_info("xmm irq0\n");
+	wake_up(&xmm->wq);
 	xmm7360_dump(xmm);
+	if (xmm->td_ring) {
+		for (id=0; id<8; id++) {
+			if (xmm->qp[id].open)
+				wake_up(&xmm->qp[id].wq);
+		}
+	}
+
 	return IRQ_HANDLED;
 }
 
@@ -223,11 +536,67 @@ static irq_handler_t xmm7360_irq_handlers[] = {
 	xmm7360_irq,
 };
 
+static void xmm7360_remove(struct pci_dev *dev)
+{
+	struct xmm_dev *xmm = pci_get_drvdata(dev);
+	int i;
+
+	for (i=0; i<8; i++) {	// XXX
+		if (xmm->qp[i].xmm) {
+			cdev_del(&xmm->qp[i].cdev);
+			device_unregister(&xmm->qp[i].dev);
+		}
+	}
+	xmm7360_cmd_ring_free(xmm);
+
+	for (i=0; i<4; i++) {
+		if (xmm->irq[i])
+			free_irq(xmm->irq[i], xmm);
+	}
+	pci_free_irq_vectors(dev);
+	pci_release_region(dev, 0);
+	pci_release_region(dev, 2);
+	pci_disable_device(dev);
+	kfree(xmm);
+}
+
+static void xmm7360_cdev_dev_release(struct device *dev)
+{
+}
+
+static int xmm7360_create_cdev(struct xmm_dev *xmm, int num)
+{
+	struct queue_pair *qp = &xmm->qp[num];
+	int ret;
+
+	qp->xmm = xmm;
+	qp->num = num;
+	qp->open = 0;
+
+	spin_lock_init(&qp->lock);
+	init_waitqueue_head(&qp->wq);
+	cdev_init(&qp->cdev, &xmm7360_fops);
+	qp->cdev.owner = THIS_MODULE;
+	device_initialize(&qp->dev);
+	qp->dev.devt = MKDEV(MAJOR(xmm_base), num); // XXX multiple cards
+	qp->dev.parent = &xmm->pci_dev->dev;
+	qp->dev.release = xmm7360_cdev_dev_release;
+	dev_set_name(&qp->dev, "xmm%d", num);
+	dev_set_drvdata(&qp->dev, qp);
+	ret = cdev_device_add(&qp->cdev, &qp->dev);
+	if (ret) {
+		pr_err("cdev_device_add: %d\n", ret);
+		return ret;
+	}
+	return 0;
+}
+
 static int xmm7360_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
 	struct xmm_dev *xmm = kzalloc(sizeof(struct xmm_dev), GFP_KERNEL);
 	int i, ret;
 	u32 status;
+	struct device *device;
 
 	xmm->pci_dev = dev;
 	xmm->dev = &dev->dev;
@@ -272,6 +641,7 @@ static int xmm7360_probe(struct pci_dev *dev, const struct pci_device_id *id)
 			goto fail;
 		}
 	}
+	init_waitqueue_head(&xmm->wq);
 
 	pci_set_drvdata(dev, xmm);
 
@@ -308,38 +678,17 @@ static int xmm7360_probe(struct pci_dev *dev, const struct pci_device_id *id)
 		goto fail;
 	}
 
+	for (i=0; i<8; i++) {
+		ret = xmm7360_create_cdev(xmm, i);
+		if (ret)
+			goto fail;
+	}
+
 	return ret;
 
 fail:
-	xmm7360_cmd_ring_free(xmm);
-
-	for (i=0; i<4; i++) {
-		if (xmm->irq[i])
-			free_irq(xmm->irq[i], xmm);
-	}
-	pci_release_region(dev, 2);
-	pci_release_region(dev, 0);
-	pci_free_irq_vectors(dev);
-	pci_disable_device(dev);
-
-	kfree(xmm);
+	xmm7360_remove(dev);
 	return ret;
-}
-
-static void xmm7360_remove(struct pci_dev *dev) {
-	struct xmm_dev *xmm = pci_get_drvdata(dev);
-	int i;
-	xmm7360_cmd_ring_free(xmm);
-
-	for (i=0; i<4; i++) {
-		if (xmm->irq[i])
-			free_irq(xmm->irq[i], xmm);
-	}
-	pci_free_irq_vectors(dev);
-	pci_release_region(dev, 0);
-	pci_release_region(dev, 2);
-	pci_disable_device(dev);
-	kfree(xmm);
 }
 
 static struct pci_driver xmm7360_driver = {
@@ -352,6 +701,9 @@ static struct pci_driver xmm7360_driver = {
 static int xmm7360_init(void)
 {
 	int ret;
+	ret = alloc_chrdev_region(&xmm_base, 0, 8, "xmm");
+	if (ret)
+		return ret;
 
 	ret = pci_register_driver(&xmm7360_driver);
 	if (ret)
@@ -362,6 +714,8 @@ static int xmm7360_init(void)
 
 static void xmm7360_exit(void)
 {
+	pr_err("xmm7360_exit\n");
+	unregister_chrdev_region(xmm_base, 8);
 	pci_unregister_driver(&xmm7360_driver);
 }
 
