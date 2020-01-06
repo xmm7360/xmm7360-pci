0460a19d03e574c7c66448d5e8d76412e691a1ae
James Wah <james@laird-wah.net>
2020-01-06 21:45:02 +1100

    rpc.py: add unpacker, demonstrate getting IP+DNS

--- rpc/rpc.py
+++ rpc/rpc.py
@@ -7,6 +7,7 @@ import time
 import struct
 import itertools
 import signal
+import ipaddress
 
 def asn_int4(val):
     return b'\x02\x04' + struct.pack('>L', val)
@@ -59,7 +60,6 @@ class XMMRPC(object):
         assert total_length + 4 == len(header) + len(body)
 
         self.response_event.clear()
-        print(binascii.hexlify(header + body))
         ret = os.write(self.fp, header + body)
         if ret < len(header + body):
             print("write error: %d", ret)
@@ -156,6 +156,49 @@ def _pack_string(val, fmt, elem_type):
     field += b'\0'*padding
     return field
 
+def take_asn_int(data):
+    assert data.pop(0) == 0x02
+    l = data.pop(0)
+    val = 0
+    for i in range(l):
+        val <<= 8
+        val |= data.pop(0)
+    return val
+
+def unpack(fmt, data):
+    data = bytearray(data)
+    out = []
+    for ch in fmt:
+        if ch == 'n':
+            val = take_asn_int(data)
+            out.append(val)
+        elif ch == 's':
+            t = data.pop(0)
+            assert t in [0x55, 0x56, 0x57]
+            valid = data.pop(0)
+            if valid & 0x80:    # Variable length!
+                value = 0
+                for byte in range(valid & 0xf): # lol
+                    value |= data.pop(0) << (byte*8)
+                valid = value
+            if t == 0x56:
+                valid <<= 1
+            elif t == 0x57:
+                valid <<= 2
+            count = take_asn_int(data)   # often equals valid + padding, but sometimes not
+            padding = take_asn_int(data)
+            if count:
+                assert count == (valid + padding)
+                field_size = count
+            else:
+                field_size = valid
+            payload = data[:valid]
+            data = data[valid + padding:]
+            out.append(payload)
+        else:
+            raise ValueError("unknown format char %s" % ch)
+
+    return out
 
 def pack(fmt, *args):
     out = b''
@@ -185,6 +228,12 @@ def pack(fmt, *args):
 
     return out
 
+def bytes_to_ipv4(data):
+    return ipaddress.IPv4Address(int(binascii.hexlify(data), 16))
+
+def bytes_to_ipv6(data):
+    return ipaddress.IPv6Address(int(binascii.hexlify(data), 16))
+
 def pack_UtaMsCallPsAttachApnConfigReq(apn):
     apn_string = bytearray(101)
     apn_string[:len(apn)] = apn.encode('ascii')
@@ -199,6 +248,32 @@ def pack_UtaMsNetAttachReq():
 
 def pack_UtaMsCallPsGetNegIpAddrReq():
     return pack('BLL', 0, 0, 0)
+
+def unpack_UtaMsCallPsGetNegIpAddrReq(data):
+    _, addresses, _, _, _, _ = unpack('nsnnnn', data)
+    a1 = bytes_to_ipv4(addresses[:4])
+    a2 = bytes_to_ipv4(addresses[4:8])
+    a3 = bytes_to_ipv4(addresses[8:12])
+
+    return a1, a2, a3
+
+
+def pack_UtaMsCallPsGetNegotiatedDnsReq():
+    return pack('BLL', 0, 0, 0)
+
+def unpack_UtaMsCallPsGetNegotiatedDnsReq(data):
+    v4 = []
+    v6 = []
+    vals = unpack('n' + 'sn'*16 + 'nsnnnn', data)
+    for i in range(16):
+        address, typ = vals[2*i+1:2*i+3]
+        if typ == 1:
+            v4.append(bytes_to_ipv4(address[:4]))
+        elif typ == 2:
+            v6.append(bytes_to_ipv6(address[:16]))
+
+    return {'v4': v4, 'v6': v6}
+
 
 def pack_UtaMsCallPsConnectReq():
     return pack('BLLL', 0, 6, 0, 0)
