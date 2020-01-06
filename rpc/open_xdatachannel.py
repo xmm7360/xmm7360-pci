8f519e51f8d82e9b2c376472735104e30af8c693
James Wah <james@laird-wah.net>
2020-01-06 21:56:41 +1100

    open_xdatachannel: replace the bodgy csr request with a nice one

--- rpc/open_xdatachannel.py
+++ rpc/open_xdatachannel.py
@@ -22,14 +22,14 @@ ip = r.execute(UtaMsCallPsGetNegIpAddrReq, rpc.pack_UtaMsCallPsGetNegIpAddrReq()
 print(rpc.unpack_UtaMsCallPsGetNegIpAddrReq(ip[1]))
 dns = r.execute(UtaMsCallPsGetNegotiatedDnsReq, rpc.pack_UtaMsCallPsGetNegotiatedDnsReq(), is_async=True)
 print(rpc.unpack_UtaMsCallPsGetNegotiatedDnsReq(dns[1]))
-r.execute(UtaMsCallPsConnectReq, rpc.pack_UtaMsCallPsConnectReq(), is_async=True)
+
+# this gives us way too much stuff, which we need
+pscr = r.execute(UtaMsCallPsConnectReq, rpc.pack_UtaMsCallPsConnectReq(), is_async=True)
+# this gives us a handle we need
 dcr = r.execute(UtaRPCPsConnectToDatachannelReq, rpc.pack_UtaRPCPsConnectToDatachannelReq())
-print("dcr req done", binascii.hexlify(dcr[1]))
 
+csr_req = pscr[1][:-6] + dcr[1] + b'\x02\x04\0\0\0\0'
 
-csr = bytearray(binascii.unhexlify('020100020400000000020400000000020400000000020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020100020400000000551402040000001702040000000300000000000000000000000000000000000000000000000204FFFFFFFF0204FFFFFFFF0204000000010204000000030204000000050201000201FA0201000201FA0201000201000201FF0201FF02010202010A0201030201070201FE0201FE02019602010302010202010302010102011F0201090201020201040201020201050204000000055514020400000014020400000000EC42632F8411A11D0AD5862E0000000000000000020400000000020400000000020400020016020400000000'))
-# XXX also contains the ConnectToDatachannelReq handle
-csr[264:264+12] = ips
-r.execute(UtaRPCPSConnectSetupReq, csr)
+r.execute(UtaRPCPSConnectSetupReq, csr_req)
 
 r.stop()
