039aa06137bbe7f86516e929c2268304da2cbbb1
James Wah <james@laird-wah.net>
2020-01-07 20:11:40 +1100

    open_xdatachannel update for unthreaded rpc

--- rpc/open_xdatachannel.py
+++ rpc/open_xdatachannel.py
@@ -23,10 +23,10 @@ r.execute(UtaMsSimOpenReq)
 r.execute(UtaMsCallPsAttachApnConfigReq, rpc.pack_UtaMsCallPsAttachApnConfigReq("telstra.internet"), is_async=True)
 r.execute(UtaMsNetAttachReq, rpc.pack_UtaMsNetAttachReq(), is_async=True)
 ip = r.execute(UtaMsCallPsGetNegIpAddrReq, rpc.pack_UtaMsCallPsGetNegIpAddrReq(), is_async=True)
-ip_values = rpc.unpack_UtaMsCallPsGetNegIpAddrReq(ip[1])
+ip_values = rpc.unpack_UtaMsCallPsGetNegIpAddrReq(ip['body'])
 
 dns = r.execute(UtaMsCallPsGetNegotiatedDnsReq, rpc.pack_UtaMsCallPsGetNegotiatedDnsReq(), is_async=True)
-dns_values = rpc.unpack_UtaMsCallPsGetNegotiatedDnsReq(dns[1])
+dns_values = rpc.unpack_UtaMsCallPsGetNegotiatedDnsReq(dns['body'])
 
 print(ip_values)
 print(dns_values)
@@ -53,8 +53,6 @@ pscr = r.execute(UtaMsCallPsConnectReq, rpc.pack_UtaMsCallPsConnectReq(), is_asy
 # this gives us a handle we need
 dcr = r.execute(UtaRPCPsConnectToDatachannelReq, rpc.pack_UtaRPCPsConnectToDatachannelReq())
 
-csr_req = pscr[1][:-6] + dcr[1] + b'\x02\x04\0\0\0\0'
+csr_req = pscr['body'][:-6] + dcr['body'] + b'\x02\x04\0\0\0\0'
 
 r.execute(UtaRPCPSConnectSetupReq, csr_req)
-
-r.stop()
